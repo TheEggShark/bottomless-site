9c451e0610fc43e093f111f1b2f483a366ede1ab
TheEggShark <ther3al3ggshark@gmail.com>
2023-04-04 17:02:38 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs};
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+    println!("Hello, world!");
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request =  buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect::<Vec<String>>();
+    
+    println!("{:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("files/index.html").unwrap();
+    let length = contents.len();
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
+}
