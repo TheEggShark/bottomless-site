fb7ba5eecd99a60196db3659e369c5ca568a574b
TheEggShark <ther3al3ggshark@gmail.com>
2023-04-04 20:21:10 -0500

    finished what the book said

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,96 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs};
+use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs, thread::JoinHandle, thread::{self, Thread}, sync::{mpsc::{Sender, self, Receiver}, Arc, Mutex}};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    let pool = ThreadPool::new(8);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream)
+        });
     }
     println!("Hello, world!");
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request =  buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect::<Vec<String>>();
-    
-    println!("{:#?}", http_request);
+    let request_line = buf_reader.lines().next();
+    let request_line = match request_line {
+        None => return,
+        Some(option) => option.unwrap(),
+    };
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("files/index.html").unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "files/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "files/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
+    fn new(size: usize) -> Self {
+        assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        (0..size).for_each(|id| workers.push(Worker::new(id, Arc::clone(&receiver))));
+
+        ThreadPool {
+            workers,
+            sender,
+        }
+    }
+
+    fn execute<F>(&self, function: F)
+    where
+        F: FnOnce() + Send + 'static,
+    {
+        let job = Box::new(function);
+
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker {
+            id, 
+            thread,
+        }
+    }
 }
