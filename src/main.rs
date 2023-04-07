5f11acd6f3c252e532a26ff5ff8a08aef5c7c745
TheEggShark <ther3al3ggshark@gmail.com>
2023-04-06 20:44:29 -0500

    other file support organization next

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use std::{net::{TcpListener, TcpStream}, io::{BufReader, BufRead, Write}, fs, thread::JoinHandle, thread::{self, Thread}, sync::{mpsc::{Sender, self, Receiver}, Arc, Mutex}};
+use std::{
+    net::{TcpListener, TcpStream},
+    io::{BufReader, BufRead, Write},
+    fs,
+    thread::{self, JoinHandle}, 
+    sync::{mpsc::{Sender, self, Receiver}, Arc, Mutex},
+    str::FromStr, fmt::Display,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -23,19 +30,31 @@ fn handle_connection(mut stream: TcpStream) {
         Some(option) => option.unwrap(),
     };
 
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "files/index.html")
+    let request_line = HTTPRequestLine::from_str(&request_line).unwrap();
+
+    println!("request_line: {:?}, {}", request_line, request_line.path == "/");
+    let path = String::from("files") + &request_line.path;
+    println!("path: {}", path);
+    let (contents, status_line) = if path == "files/" {
+        (fs::read("files/index.html").unwrap(), "HTTP/1.1 200 OK")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "files/404.html")
+        match fs::read(path) {
+            Ok(s) => (s, "HTTP/1.1 200 OK"),
+            Err(e) => {
+                println!("{}", e);
+                (fs::read("files/404.html").unwrap(), "HTTP/1.1 404 NOT FOUND")
+            }
+        }
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
     let length = contents.len();
 
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response_header =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n");
 
-    stream.write_all(response.as_bytes()).unwrap();
+    let response = [response_header.as_bytes(), &contents].concat();
+
+    stream.write_all(&response).unwrap();
 }
 
 struct ThreadPool {
@@ -93,4 +112,88 @@ impl Worker {
             thread,
         }
     }
+}
+
+fn make_code(code: u16) -> String {
+    match code {
+        200 => String::from("HTTP/1.1 200 OK"),
+        404 => String::from("HTTP/1.1 404 NOT FOUND"),
+        _ => unimplemented!(),
+    }
+}
+
+#[derive(Debug)]
+struct Response {
+    code: u16,
+    content_type: ContentType,
+}
+
+#[derive(Debug)]
+enum ContentType {
+    Image,
+    Css,
+    Html,
+}
+
+impl Display for ContentType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Image => write!(f, "image/png"),
+            Self::Css => write!(f, "text/css"),
+            Self::Html => write!(f, "text/html"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct HTTPRequestLine {
+    kind: HTTPType,
+    path: String,
+}
+
+impl FromStr for HTTPRequestLine {
+    type Err = HTTPError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut groups = s.split_whitespace();
+
+        let kind = match groups.next() {
+            None => return Err(HTTPError::InvalidRequestType),
+            Some(kind) => {
+                match kind {
+                    "GET" => HTTPType::Get,
+                    "POST" => HTTPType::Post,
+                    _ => return Err(HTTPError::InvalidRequestType),
+                }
+            }
+        };
+
+        let path = match groups.next() {
+            None => return Err(HTTPError::MissingPath),
+            Some(s) => s.to_string()
+        };
+
+        match groups.next() {
+            None => return Err(HTTPError::InvalidVersion),
+            Some(_) => {},
+        };
+
+        Ok(Self {
+            kind,
+            path,
+        })
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+enum HTTPType {
+    Post,
+    Get,
+}
+
+#[derive(Clone, Copy, Debug)]
+enum HTTPError {
+    MissingPath,
+    InvalidRequestType,
+    InvalidVersion,
+    InvalidRequestLine,
 }
